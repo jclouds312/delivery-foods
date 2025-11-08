cd1b195150a0f05ce11d99d520a7d9bf02f0095a
serviciosyventas20000 <serviciosyventas20000@gmail.com>
2025-11-08 19:06:56 +0000

    sync

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.google.gms:google-services:4.4.1")
     }
 }
 
